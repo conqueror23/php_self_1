716e7b238623257ea8ce9f475d77cd4535101a8f
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2018-07-23 14:21:08 +1000

    new copy

---
+++ conn.php
@@ -0,0 +1,48 @@
+<?php
+/*
+$servername = "localhost";
+$username ="root";
+$userp="";
+$conn =mysqli_connect($servername,$username,$userp,"uni");
+*/
+
+//mamp must use define
+
+DEFINE ('USERNAME','root');
+DEFINE('PASS','root');
+DEFINE('DBHOST','localhost');
+DEFINE('DBNAME','uni');
+
+$mysqli = new mysqli(DBHOST,USERNAME,PASS,DBNAME);
+
+//echo "predifiend things";
+
+
+if(mysqli_connect_error()){
+  echo "connected failed";
+}else{
+  echo "connected succeed";
+}
+$sql = "select * from students";
+
+$result = $mysqli->query($sql);
+
+//print_r($result);
+
+while (!$row = $sql ->fetch_row()){
+
+
+$arr[] = $row;
+  //
+  // foreach ($row as $key->$value){
+  //   echo $key;
+  //   echo $value;
+  // }
+}
+print_r($arr);
+
+
+
+$mysqli -> close();
+
+?>
